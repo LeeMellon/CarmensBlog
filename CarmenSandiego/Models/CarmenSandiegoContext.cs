597dd9db786424c462294244574b8a0041b3213b
Ian Goodrich and Stephanie Faber <ian+stephanie@>
2018-04-18 09:59:13 -0700

    create project, add models

---
+++ CarmenSandiego/Models/CarmenSandiegoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarmenSandiego.Models
+{
+    public class CarmenSandiegoContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=8889;database=carmensandiego;uid=root;pwd=root;");
+    }
+}
---
+++ CarmenSandiego/Models/Experience.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarmenSandiego.Models;
+namespace CarmenSandiego.Models
+{
+    [Table("Experiences")]
+    public class Experience
+    {
+        [Key]
+        public int ExperienceId { get; set; }
+        public string Name { get; set; }
+        public virtual Location Location { get; set; }
+        public virtual int LocationId { get; set; }
+        public virtual List<Person> People { get; set; }
+
+    }
+}
